7ad6f82b1054ac77d92276ba838100add7b5fa81
Markus Scheidgen <markus.scheidgen@gmail.com>
2015-11-16 11:32:03 +0100

    Added setup for excersise 3.

---
+++ plugins/de.hub.modsoft.1516.exercise3.twittersearch/src/de/hub/modsoft/twittersearch/twittersearch/ExampleClass.java
@@ -0,0 +1,18 @@
+/**
+ */
+package de.hub.modsoft.twittersearch.twittersearch;
+
+import org.eclipse.emf.ecore.EObject;
+
+/**
+ * <!-- begin-user-doc -->
+ * A representation of the model object '<em><b>Example Class</b></em>'.
+ * <!-- end-user-doc -->
+ *
+ *
+ * @see de.hub.modsoft.twittersearch.twittersearch.TwitterSearchPackage#getExampleClass()
+ * @model
+ * @generated
+ */
+public interface ExampleClass extends EObject {
+} // ExampleClass
