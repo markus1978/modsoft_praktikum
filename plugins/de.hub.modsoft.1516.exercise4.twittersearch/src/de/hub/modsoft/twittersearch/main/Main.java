05f067ec2c32d1ae8047dae04dffd5eb6e1b0bbf
Markus Scheidgen <markus.scheidgen@gmail.com>
2015-12-01 12:11:46 +0100

    Added skeleton for excersise 4 with meta-model musterlösung for execersie3 as meta-model for excersise 4.

---
+++ plugins/de.hub.modsoft.1516.exercise4.twittersearch/src/de/hub/modsoft/twittersearch/main/Main.java
@@ -0,0 +1,26 @@
+package de.hub.modsoft.twittersearch.main;
+
+import de.hub.modsoft.model.twittersearch.Search;
+import de.hub.modsoft.model.twittersearch.TwitterSearchFactory;
+
+public class Main {
+	
+	public static String serialize(Search search) {
+		return ""; // TODO
+	}
+	
+	public static void main(String[] args) throws Exception {
+		// search for tweets with “VW”, “abgasskandal” where tweet.retweets > 5 & tweet.hasURL
+		Search example1 = TwitterSearchFactory.eINSTANCE.createSearch();
+		// TODO
+		
+		// search for tweets with “quake” in geo(37.781157,-122.398720,1mi) before 12.11.2012 where tweet.hasImage
+		// TODO
+		
+		// search for tweets with “@realDonaldTrump” where tweet.message.contains(“hairpiece”) sort user.followers
+		// TODO
+		
+		System.out.println(serialize(example1));
+		// TODO
+	}
+}
---
+++ plugins/de.hub.modsoft.1516.exercise4.twittersearch/src/de/hub/modsoft/twittersearch/modelalt/data/Location.java
@@ -0,0 +1,27 @@
+package de.hub.modsoft.twittersearch.modelalt.data;
+
+public class Location {
+
+	private final double longitude;
+	private final double latitude;
+	private final double radius;
+	public Location(double longitude, double latitude, double radius) {
+		super();
+		this.longitude = longitude;
+		this.latitude = latitude;
+		this.radius = radius;
+	}
+	public double getLongitude() {
+		return longitude;
+	}
+	public double getLatitude() {
+		return latitude;
+	}
+	public double getRadius() {
+		return radius;
+	}
+
+	public String toString() {		
+		return "("+longitude+","+latitude+","+radius+"mi)";
+	}
+}
