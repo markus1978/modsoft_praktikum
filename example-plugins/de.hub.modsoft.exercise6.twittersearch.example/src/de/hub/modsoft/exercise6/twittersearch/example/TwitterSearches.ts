0fe6c87f0488b9d8f5d09efa883dc538c3ae4f5b
Markus Scheidgen <markus.scheidgen@gmail.com>
2016-07-12 12:14:13 +0200

    Changed example .ts file to new syntax.

--- example-plugins/de.hub.modsoft.exercise6.twittersearch.example/src/de/hub/modsoft/exercise6/twittersearch/example/TwitterSearches.ts
+++ example-plugins/de.hub.modsoft.exercise6.twittersearch.example/src/de/hub/modsoft/exercise6/twittersearch/example/TwitterSearches.ts
@@ -1,18 +1,22 @@
-configuration:
+twitter {
+	primitive Integer
+	primitive Boolean
 
-	objecttype tweet {
-		field retweets : INT
-		field favorits : INT
-		field hasImage : BOOLEAN
-		field hasURL : BOOLEAN
-		field content : STRING	
-	}
-	
-	objecttype user {
-		field follower : INT
-		field followes : INT
-		field name : STRING 
+	object Tweet {
+		field retweet_count : Integer
+		field hasImage : Boolean
+		field hasURL : Boolean
 	}
 
-searches:
-	search for tweet with "VW", "abgasskandal" where tweet.retweets > 5 & tweet.hasURL
+	object User {
+		field favourites_count : Integer
+		field followers_count : Integer
+		field friends_count : Integer
+	}
+}
+
+searches {
+	search for Tweet with "VW" where retweet_count > 5 & hasURL
+	search for Tweet with "quake" in geo(37.781157,-122.398720,100mi) where hasImage
+	search for User with "Donald" before 11.07.2016 where followers_count > 10
+}
