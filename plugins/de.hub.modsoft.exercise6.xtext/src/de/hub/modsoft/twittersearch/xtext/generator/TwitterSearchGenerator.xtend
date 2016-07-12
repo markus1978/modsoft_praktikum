0db9060537b5718d0a989d2a3770961cd143a7d9
Markus Scheidgen <markus.scheidgen@gmail.com>
2016-07-12 12:04:41 +0200

    Removed the solution for exercise7 (SS16)

--- plugins/de.hub.modsoft.exercise6.xtext/src/de/hub/modsoft/twittersearch/xtext/generator/TwitterSearchGenerator.xtend
+++ plugins/de.hub.modsoft.exercise6.xtext/src/de/hub/modsoft/twittersearch/xtext/generator/TwitterSearchGenerator.xtend
@@ -1,18 +1,9 @@
 package de.hub.modsoft.twittersearch.xtext.generator
 
-import de.hub.modsoft.twittersearch.model.BooleanCondition
-import de.hub.modsoft.twittersearch.model.IntCondition
-import de.hub.modsoft.twittersearch.model.IntOperators
-import de.hub.modsoft.twittersearch.model.Keywords
-import de.hub.modsoft.twittersearch.model.Location
 import de.hub.modsoft.twittersearch.model.Model
-import de.hub.modsoft.twittersearch.model.Search
-import de.hub.modsoft.twittersearch.model.Time
 import de.hub.modsoft.twittersearch.model.TwitterSearchPackage
 import de.hub.modsoft.twittersearch.xtext.TwitterSearchParser
 import java.io.File
-import java.text.DecimalFormat
-import java.text.SimpleDateFormat
 import org.apache.commons.io.FileUtils
 import org.eclipse.emf.ecore.EPackage
 import org.eclipse.emf.ecore.resource.Resource
@@ -45,93 +36,10 @@ class TwitterSearchGenerator implements IGenerator {
 		public class RunSearchMain {
 			
 			public static void main(String[] args) throws Exception {
-				«FOR search: model.searches»
-					{						
-						«doGenerateRunSearch(search)»
-					}
-				«ENDFOR»
+				// TODO
 			}
 		}
 	'''
-	
-	dispatch def parameter(Keywords option) {
-		return "q" -> option.keywords.map[substring(1,it.length-1)].join(" ")
-	}
-	
-	dispatch def parameter(Location option) {
-		val format = new DecimalFormat("#.000000")
-		return "geocode" -> '''«format.format(option.lon)»,«format.format(option.lat)»,«option.radius as int»«option.radiusUnit»'''
-	}
-	
-	dispatch def parameter(Time option) {
-		return "before" -> '''«new SimpleDateFormat("YYYY-MM-DD").format(option.before)»'''
-	}
-	
-	def uri(Search search) {
-		return switch (search.searchType.name) {
-			case "Tweet": "search/tweets"
-			case "User": "users/search"
-			default: throw new RuntimeException("impossible")
-		}
-	}
-	
-	dispatch def condition(BooleanCondition condition) {
-		val entity = condition.fieldDeclaration.name.substring(3).toLowerCase	+ "s"
-		return '''result.getJSONObject("entities").getJSONArray("«entity»").length() > 0'''
-	}
-	
-	def intOperator(IntOperators op) {
-		switch (op) {
-			case EQ: "=="
-			case GEQ: ">="
-			case LEQ: "<="
-			case GT: ">"
-			case LT: "<"
-		}
-	}
-
-	dispatch def condition(IntCondition condition) {
-		return '''result.getInt("«condition.fieldDeclaration.name»") «condition.operator.intOperator» «condition.operand»'''
-	}
-	
-	def doGenerateRunSearch(Search search) '''
-		GetRequest request = Unirest.get("http://jupiter.informatik.hu-berlin.de:8080/1.1/«search.uri».json");
-		request.queryString("count", "100");
-		«FOR option:search.options.map[parameter]»
-			request.queryString("«option.key»", "«option.value»");
-		«ENDFOR»
-		System.out.println(request.getUrl());
-		
-		HttpResponse<JsonNode> response = request.asJson();
-		if (response.getStatus() != 200) {
-			System.out.println("Something went wrong.");
-		}
-		
-		«IF search.searchType.name == "User"»
-			JSONArray results = response.getBody().getArray();
-		«ELSE»
-			JSONArray results = response.getBody().getObject().getJSONArray("statuses");
-		«ENDIF»
-		
-		int count = 0;
-		for (int i = 0; i < results.length(); i++) {
-			JSONObject result = results.getJSONObject(i);
-			boolean fulfilsConditions = true;
-			«FOR condition:search.conditions»
-				fulfilsConditions &= «condition(condition)»;
-			«ENDFOR»
-			if (fulfilsConditions) {
-				«IF search.searchType.name == "User"»
-					System.out.print(result.getString("name") + ": ");
-					System.out.println("\"" + result.getString("screen_name") + "\"");
-				«ELSE»
-					System.out.print(result.getJSONObject("user").getString("name") + ": ");
-					System.out.println("\"" + result.getString("text") + "\"");				
-				«ENDIF»
-				if (count++ >= 3) break;				
-			}
-		}
-	'''	
 
 	static def void main(String[] args) {
 		EPackage.Registry.INSTANCE.put(TwitterSearchPackage.eINSTANCE.nsURI, TwitterSearchPackage.eINSTANCE)
--- plugins/de.hub.modsoft.exercise6.xtext/xtend-gen/de/hub/modsoft/twittersearch/xtext/generator/TwitterSearchGenerator.java
+++ plugins/de.hub.modsoft.exercise6.xtext/xtend-gen/de/hub/modsoft/twittersearch/xtext/generator/TwitterSearchGenerator.java
@@ -1,27 +1,9 @@
 package de.hub.modsoft.twittersearch.xtext.generator;
 
-import com.google.common.base.Objects;
-import de.hub.modsoft.twittersearch.model.BooleanCondition;
-import de.hub.modsoft.twittersearch.model.Condition;
-import de.hub.modsoft.twittersearch.model.DistanceUnit;
-import de.hub.modsoft.twittersearch.model.IntCondition;
-import de.hub.modsoft.twittersearch.model.IntOperators;
-import de.hub.modsoft.twittersearch.model.Keywords;
-import de.hub.modsoft.twittersearch.model.Location;
 import de.hub.modsoft.twittersearch.model.Model;
-import de.hub.modsoft.twittersearch.model.Search;
-import de.hub.modsoft.twittersearch.model.SearchOption;
-import de.hub.modsoft.twittersearch.model.Time;
-import de.hub.modsoft.twittersearch.model.TwitterFieldDeclaration;
-import de.hub.modsoft.twittersearch.model.TwitterObjectTypeDeclaration;
 import de.hub.modsoft.twittersearch.model.TwitterSearchPackage;
 import de.hub.modsoft.twittersearch.xtext.TwitterSearchParser;
 import java.io.File;
-import java.text.DecimalFormat;
-import java.text.SimpleDateFormat;
-import java.util.Arrays;
-import java.util.Date;
-import java.util.List;
 import org.apache.commons.io.FileUtils;
 import org.eclipse.emf.common.util.EList;
 import org.eclipse.emf.ecore.EObject;
@@ -31,10 +13,6 @@ import org.eclipse.xtend2.lib.StringConcatenation;
 import org.eclipse.xtext.generator.IFileSystemAccess;
 import org.eclipse.xtext.generator.IGenerator;
 import org.eclipse.xtext.xbase.lib.Exceptions;
-import org.eclipse.xtext.xbase.lib.Functions.Function1;
-import org.eclipse.xtext.xbase.lib.IterableExtensions;
-import org.eclipse.xtext.xbase.lib.ListExtensions;
-import org.eclipse.xtext.xbase.lib.Pair;
 
 /**
  * Generates code from your model files on save.
@@ -78,239 +56,8 @@ public class TwitterSearchGenerator implements IGenerator {
     _builder.append("\t");
     _builder.append("public static void main(String[] args) throws Exception {");
     _builder.newLine();
-    {
-      EList<Search> _searches = model.getSearches();
-      for(final Search search : _searches) {
-        _builder.append("\t\t");
-        _builder.append("{\t\t\t\t\t\t");
-        _builder.newLine();
-        _builder.append("\t\t");
-        _builder.append("\t");
-        CharSequence _doGenerateRunSearch = this.doGenerateRunSearch(search);
-        _builder.append(_doGenerateRunSearch, "\t\t\t");
-        _builder.newLineIfNotEmpty();
-        _builder.append("\t\t");
-        _builder.append("}");
-        _builder.newLine();
-      }
-    }
-    _builder.append("\t");
-    _builder.append("}");
-    _builder.newLine();
-    _builder.append("}");
-    _builder.newLine();
-    return _builder;
-  }
-  
-  protected Pair<String, String> _parameter(final Keywords option) {
-    EList<String> _keywords = option.getKeywords();
-    final Function1<String, String> _function = (String it) -> {
-      int _length = it.length();
-      int _minus = (_length - 1);
-      return it.substring(1, _minus);
-    };
-    List<String> _map = ListExtensions.<String, String>map(_keywords, _function);
-    String _join = IterableExtensions.join(_map, " ");
-    return Pair.<String, String>of("q", _join);
-  }
-  
-  protected Pair<String, String> _parameter(final Location option) {
-    final DecimalFormat format = new DecimalFormat("#.000000");
-    StringConcatenation _builder = new StringConcatenation();
-    double _lon = option.getLon();
-    String _format = format.format(_lon);
-    _builder.append(_format, "");
-    _builder.append(",");
-    double _lat = option.getLat();
-    String _format_1 = format.format(_lat);
-    _builder.append(_format_1, "");
-    _builder.append(",");
-    double _radius = option.getRadius();
-    _builder.append(((int) _radius), "");
-    DistanceUnit _radiusUnit = option.getRadiusUnit();
-    _builder.append(_radiusUnit, "");
-    return Pair.<String, String>of("geocode", _builder.toString());
-  }
-  
-  protected Pair<String, String> _parameter(final Time option) {
-    StringConcatenation _builder = new StringConcatenation();
-    SimpleDateFormat _simpleDateFormat = new SimpleDateFormat("YYYY-MM-DD");
-    Date _before = option.getBefore();
-    String _format = _simpleDateFormat.format(_before);
-    _builder.append(_format, "");
-    return Pair.<String, String>of("before", _builder.toString());
-  }
-  
-  public String uri(final Search search) {
-    String _switchResult = null;
-    TwitterObjectTypeDeclaration _searchType = search.getSearchType();
-    String _name = _searchType.getName();
-    switch (_name) {
-      case "Tweet":
-        _switchResult = "search/tweets";
-        break;
-      case "User":
-        _switchResult = "users/search";
-        break;
-      default:
-        throw new RuntimeException("impossible");
-    }
-    return _switchResult;
-  }
-  
-  protected String _condition(final BooleanCondition condition) {
-    TwitterFieldDeclaration _fieldDeclaration = condition.getFieldDeclaration();
-    String _name = _fieldDeclaration.getName();
-    String _substring = _name.substring(3);
-    String _lowerCase = _substring.toLowerCase();
-    final String entity = (_lowerCase + "s");
-    StringConcatenation _builder = new StringConcatenation();
-    _builder.append("result.getJSONObject(\"entities\").getJSONArray(\"");
-    _builder.append(entity, "");
-    _builder.append("\").length() > 0");
-    return _builder.toString();
-  }
-  
-  public String intOperator(final IntOperators op) {
-    String _switchResult = null;
-    if (op != null) {
-      switch (op) {
-        case EQ:
-          _switchResult = "==";
-          break;
-        case GEQ:
-          _switchResult = ">=";
-          break;
-        case LEQ:
-          _switchResult = "<=";
-          break;
-        case GT:
-          _switchResult = ">";
-          break;
-        case LT:
-          _switchResult = "<";
-          break;
-        default:
-          break;
-      }
-    }
-    return _switchResult;
-  }
-  
-  protected String _condition(final IntCondition condition) {
-    StringConcatenation _builder = new StringConcatenation();
-    _builder.append("result.getInt(\"");
-    TwitterFieldDeclaration _fieldDeclaration = condition.getFieldDeclaration();
-    String _name = _fieldDeclaration.getName();
-    _builder.append(_name, "");
-    _builder.append("\") ");
-    IntOperators _operator = condition.getOperator();
-    String _intOperator = this.intOperator(_operator);
-    _builder.append(_intOperator, "");
-    _builder.append(" ");
-    int _operand = condition.getOperand();
-    _builder.append(_operand, "");
-    return _builder.toString();
-  }
-  
-  public CharSequence doGenerateRunSearch(final Search search) {
-    StringConcatenation _builder = new StringConcatenation();
-    _builder.append("GetRequest request = Unirest.get(\"http://jupiter.informatik.hu-berlin.de:8080/1.1/");
-    String _uri = this.uri(search);
-    _builder.append(_uri, "");
-    _builder.append(".json\");");
-    _builder.newLineIfNotEmpty();
-    _builder.append("request.queryString(\"count\", \"100\");");
-    _builder.newLine();
-    {
-      EList<SearchOption> _options = search.getOptions();
-      final Function1<SearchOption, Pair<String, String>> _function = (SearchOption it) -> {
-        return this.parameter(it);
-      };
-      List<Pair<String, String>> _map = ListExtensions.<SearchOption, Pair<String, String>>map(_options, _function);
-      for(final Pair<String, String> option : _map) {
-        _builder.append("request.queryString(\"");
-        String _key = option.getKey();
-        _builder.append(_key, "");
-        _builder.append("\", \"");
-        String _value = option.getValue();
-        _builder.append(_value, "");
-        _builder.append("\");");
-        _builder.newLineIfNotEmpty();
-      }
-    }
-    _builder.append("System.out.println(request.getUrl());");
-    _builder.newLine();
-    _builder.newLine();
-    _builder.append("HttpResponse<JsonNode> response = request.asJson();");
-    _builder.newLine();
-    _builder.append("if (response.getStatus() != 200) {");
-    _builder.newLine();
-    _builder.append("\t");
-    _builder.append("System.out.println(\"Something went wrong.\");");
-    _builder.newLine();
-    _builder.append("}");
-    _builder.newLine();
-    _builder.newLine();
-    {
-      TwitterObjectTypeDeclaration _searchType = search.getSearchType();
-      String _name = _searchType.getName();
-      boolean _equals = Objects.equal(_name, "User");
-      if (_equals) {
-        _builder.append("JSONArray results = response.getBody().getArray();");
-        _builder.newLine();
-      } else {
-        _builder.append("JSONArray results = response.getBody().getObject().getJSONArray(\"statuses\");");
-        _builder.newLine();
-      }
-    }
-    _builder.newLine();
-    _builder.append("int count = 0;");
-    _builder.newLine();
-    _builder.append("for (int i = 0; i < results.length(); i++) {");
-    _builder.newLine();
-    _builder.append("\t");
-    _builder.append("JSONObject result = results.getJSONObject(i);");
-    _builder.newLine();
-    _builder.append("\t");
-    _builder.append("boolean fulfilsConditions = true;");
-    _builder.newLine();
-    {
-      EList<Condition> _conditions = search.getConditions();
-      for(final Condition condition : _conditions) {
-        _builder.append("\t");
-        _builder.append("fulfilsConditions &= ");
-        String _condition = this.condition(condition);
-        _builder.append(_condition, "\t");
-        _builder.append(";");
-        _builder.newLineIfNotEmpty();
-      }
-    }
-    _builder.append("\t");
-    _builder.append("if (fulfilsConditions) {");
-    _builder.newLine();
-    {
-      TwitterObjectTypeDeclaration _searchType_1 = search.getSearchType();
-      String _name_1 = _searchType_1.getName();
-      boolean _equals_1 = Objects.equal(_name_1, "User");
-      if (_equals_1) {
-        _builder.append("\t\t");
-        _builder.append("System.out.print(result.getString(\"name\") + \": \");");
-        _builder.newLine();
-        _builder.append("\t\t");
-        _builder.append("System.out.println(\"\\\"\" + result.getString(\"screen_name\") + \"\\\"\");");
-        _builder.newLine();
-      } else {
-        _builder.append("\t\t");
-        _builder.append("System.out.print(result.getJSONObject(\"user\").getString(\"name\") + \": \");");
-        _builder.newLine();
-        _builder.append("\t\t");
-        _builder.append("System.out.println(\"\\\"\" + result.getString(\"text\") + \"\\\"\");\t\t\t\t");
-        _builder.newLine();
-      }
-    }
     _builder.append("\t\t");
-    _builder.append("if (count++ >= 3) break;\t\t\t\t");
+    _builder.append("// TODO");
     _builder.newLine();
     _builder.append("\t");
     _builder.append("}");
@@ -390,30 +137,6 @@ public class TwitterSearchGenerator implements IGenerator {
       FileUtils.write(_file, code);
     } catch (Throwable _e) {
       throw Exceptions.sneakyThrow(_e);
-    }
-  }
-  
-  public Pair<String, String> parameter(final SearchOption option) {
-    if (option instanceof Keywords) {
-      return _parameter((Keywords)option);
-    } else if (option instanceof Location) {
-      return _parameter((Location)option);
-    } else if (option instanceof Time) {
-      return _parameter((Time)option);
-    } else {
-      throw new IllegalArgumentException("Unhandled parameter types: " +
-        Arrays.<Object>asList(option).toString());
-    }
-  }
-  
-  public String condition(final Condition condition) {
-    if (condition instanceof BooleanCondition) {
-      return _condition((BooleanCondition)condition);
-    } else if (condition instanceof IntCondition) {
-      return _condition((IntCondition)condition);
-    } else {
-      throw new IllegalArgumentException("Unhandled parameter types: " +
-        Arrays.<Object>asList(condition).toString());
     }
   }
 }
