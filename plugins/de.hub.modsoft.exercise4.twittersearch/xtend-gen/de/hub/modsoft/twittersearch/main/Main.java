de891b03457546b808e375f4feabff3d3dba72b5
Markus Scheidgen <markus.scheidgen@gmail.com>
2016-11-28 11:38:51 +0100

    Preared execeise 4, contraints, for WS 1617.

--- plugins/de.hub.modsoft.exercise4.twittersearch/xtend-gen/de/hub/modsoft/twittersearch/main/Main.java
+++ plugins/de.hub.modsoft.exercise4.twittersearch/xtend-gen/de/hub/modsoft/twittersearch/main/Main.java
@@ -1,39 +1,28 @@
 package de.hub.modsoft.twittersearch.main;
 
-import com.google.common.base.Objects;
 import de.hub.modsoft.twittersearch.BooleanBinaryOp;
-import de.hub.modsoft.twittersearch.BooleanCondition;
 import de.hub.modsoft.twittersearch.BooleanExpression;
 import de.hub.modsoft.twittersearch.Condition;
 import de.hub.modsoft.twittersearch.Dot;
 import de.hub.modsoft.twittersearch.FieldExpression;
 import de.hub.modsoft.twittersearch.FieldReference;
-import de.hub.modsoft.twittersearch.IntCondition;
 import de.hub.modsoft.twittersearch.Model;
 import de.hub.modsoft.twittersearch.Not;
 import de.hub.modsoft.twittersearch.Search;
-import de.hub.modsoft.twittersearch.Twitter;
 import de.hub.modsoft.twittersearch.TwitterFieldDeclaration;
 import de.hub.modsoft.twittersearch.TwitterObjectTypeDeclaration;
 import de.hub.modsoft.twittersearch.TwitterSearchPackage;
-import de.hub.modsoft.twittersearch.Type;
 import java.util.Map;
-import java.util.Set;
 import java.util.function.Consumer;
 import org.eclipse.emf.common.util.EList;
-import org.eclipse.emf.common.util.TreeIterator;
 import org.eclipse.emf.common.util.URI;
-import org.eclipse.emf.ecore.EClass;
 import org.eclipse.emf.ecore.EObject;
 import org.eclipse.emf.ecore.EPackage;
 import org.eclipse.emf.ecore.resource.Resource;
 import org.eclipse.emf.ecore.resource.impl.ResourceSetImpl;
 import org.eclipse.emf.ecore.xmi.impl.XMIResourceFactoryImpl;
 import org.eclipse.xtend2.lib.StringConcatenation;
-import org.eclipse.xtext.xbase.lib.Functions.Function1;
 import org.eclipse.xtext.xbase.lib.InputOutput;
-import org.eclipse.xtext.xbase.lib.IterableExtensions;
-import org.eclipse.xtext.xbase.lib.IteratorExtensions;
 
 @SuppressWarnings("all")
 public class Main {
@@ -42,11 +31,10 @@ public class Main {
    */
   public static TwitterFieldDeclaration fieldDeclaration(final FieldExpression fieldExpr) {
     if ((fieldExpr instanceof FieldReference)) {
-      return ((FieldReference)fieldExpr).getFieldDeclaration();
+      return null;
     } else {
       if ((fieldExpr instanceof Dot)) {
-        FieldReference _right = ((Dot)fieldExpr).getRight();
-        return _right.getFieldDeclaration();
+        return null;
       }
     }
     return null;
@@ -58,38 +46,11 @@ public class Main {
   public static boolean fieldExists(final FieldExpression fieldExpr, final TwitterObjectTypeDeclaration objectType) {
     boolean _xifexpression = false;
     if ((fieldExpr instanceof Dot)) {
-      boolean _xblockexpression = false;
-      {
-        FieldExpression _left = ((Dot)fieldExpr).getLeft();
-        TwitterFieldDeclaration _fieldDeclaration = Main.fieldDeclaration(_left);
-        final Type leftFieldType = _fieldDeclaration.getType();
-        boolean _xifexpression_1 = false;
-        if ((leftFieldType instanceof TwitterObjectTypeDeclaration)) {
-          FieldReference _right = ((Dot)fieldExpr).getRight();
-          _xifexpression_1 = Main.fieldExists(_right, ((TwitterObjectTypeDeclaration)leftFieldType));
-        } else {
-          boolean _and = false;
-          if (!false) {
-            _and = false;
-          } else {
-            FieldExpression _left_1 = ((Dot)fieldExpr).getLeft();
-            boolean _fieldExists = Main.fieldExists(_left_1, objectType);
-            _and = _fieldExists;
-          }
-          _xifexpression_1 = _and;
-        }
-        _xblockexpression = _xifexpression_1;
-      }
-      _xifexpression = _xblockexpression;
+      _xifexpression = false;
     } else {
       boolean _xifexpression_1 = false;
       if ((fieldExpr instanceof FieldReference)) {
-        EList<TwitterFieldDeclaration> _fields = objectType.getFields();
-        final Function1<TwitterFieldDeclaration, Boolean> _function = (TwitterFieldDeclaration it) -> {
-          TwitterFieldDeclaration _fieldDeclaration = ((FieldReference)fieldExpr).getFieldDeclaration();
-          return Boolean.valueOf(Objects.equal(it, _fieldDeclaration));
-        };
-        _xifexpression_1 = IterableExtensions.<TwitterFieldDeclaration>exists(_fields, _function);
+        _xifexpression_1 = false;
       } else {
         _xifexpression_1 = false;
       }
@@ -103,35 +64,7 @@ public class Main {
    * all fields within the contained field expression exist in the context of the given search type.
    */
   public static boolean checkType(final Condition condition, final TwitterObjectTypeDeclaration searchType) {
-    boolean _and = false;
-    FieldExpression _fieldExpression = condition.getFieldExpression();
-    boolean _fieldExists = Main.fieldExists(_fieldExpression, searchType);
-    if (!_fieldExists) {
-      _and = false;
-    } else {
-      boolean _xifexpression = false;
-      if ((condition instanceof IntCondition)) {
-        FieldExpression _fieldExpression_1 = ((IntCondition)condition).getFieldExpression();
-        TwitterFieldDeclaration _fieldDeclaration = Main.fieldDeclaration(_fieldExpression_1);
-        Type _type = _fieldDeclaration.getType();
-        String _name = _type.getName();
-        _xifexpression = Objects.equal(_name, "Integer");
-      } else {
-        boolean _xifexpression_1 = false;
-        if ((condition instanceof BooleanCondition)) {
-          FieldExpression _fieldExpression_2 = ((BooleanCondition)condition).getFieldExpression();
-          TwitterFieldDeclaration _fieldDeclaration_1 = Main.fieldDeclaration(_fieldExpression_2);
-          Type _type_1 = _fieldDeclaration_1.getType();
-          String _name_1 = _type_1.getName();
-          _xifexpression_1 = Objects.equal(_name_1, "Boolean");
-        } else {
-          _xifexpression_1 = false;
-        }
-        _xifexpression = _xifexpression_1;
-      }
-      _and = _xifexpression;
-    }
-    return _and;
+    return false;
   }
   
   /**
@@ -144,24 +77,13 @@ public class Main {
     if (!_matched) {
       if (expr instanceof Not) {
         _matched=true;
-        BooleanExpression _operand = ((Not)expr).getOperand();
-        _switchResult = Main.checkTypes(_operand, searchType);
+        _switchResult = false;
       }
     }
     if (!_matched) {
       if (expr instanceof BooleanBinaryOp) {
         _matched=true;
-        boolean _and = false;
-        BooleanExpression _left = ((BooleanBinaryOp)expr).getLeft();
-        boolean _checkTypes = Main.checkTypes(_left, searchType);
-        if (!_checkTypes) {
-          _and = false;
-        } else {
-          BooleanExpression _right = ((BooleanBinaryOp)expr).getRight();
-          boolean _checkTypes_1 = Main.checkTypes(_right, searchType);
-          _and = _checkTypes_1;
-        }
-        _switchResult = _and;
+        _switchResult = false;
       }
     }
     if (!_matched) {
@@ -199,53 +121,13 @@ public class Main {
     _searches.forEach(_function);
     InputOutput.<String>println("Questions");
     InputOutput.<String>println("Do all searches search for tweets?");
-    EList<Search> _searches_1 = model.getSearches();
-    final Function1<Search, Boolean> _function_1 = (Search it) -> {
-      TwitterObjectTypeDeclaration _searchType = it.getSearchType();
-      String _name = _searchType.getName();
-      return Boolean.valueOf(Objects.equal(_name, "Tweet"));
-    };
-    boolean _forall = IterableExtensions.<Search>forall(_searches_1, _function_1);
-    InputOutput.<Boolean>println(Boolean.valueOf(_forall));
+    InputOutput.<String>println("TODO");
     InputOutput.<String>println("Does at least one search search for users?");
-    EList<Search> _searches_2 = model.getSearches();
-    final Function1<Search, TwitterObjectTypeDeclaration> _function_2 = (Search it) -> {
-      return it.getSearchType();
-    };
-    Iterable<TwitterObjectTypeDeclaration> _collect = de.hub.modsoft.twittersearch.main.IterableExtensions.<Search, TwitterObjectTypeDeclaration>collect(_searches_2, _function_2);
-    final Function1<TwitterObjectTypeDeclaration, Boolean> _function_3 = (TwitterObjectTypeDeclaration it) -> {
-      String _name = it.getName();
-      return Boolean.valueOf(Objects.equal(_name, "User"));
-    };
-    boolean _exists = IterableExtensions.<TwitterObjectTypeDeclaration>exists(_collect, _function_3);
-    InputOutput.<Boolean>println(Boolean.valueOf(_exists));
+    InputOutput.<String>println("TODO");
     InputOutput.<String>println("How many different EClasses do all used search types instantiate?");
-    EList<Search> _searches_3 = model.getSearches();
-    final Function1<Search, EClass> _function_4 = (Search it) -> {
-      TwitterObjectTypeDeclaration _searchType = it.getSearchType();
-      return _searchType.eClass();
-    };
-    Iterable<EClass> _collect_1 = de.hub.modsoft.twittersearch.main.IterableExtensions.<Search, EClass>collect(_searches_3, _function_4);
-    Set<EClass> _set = IterableExtensions.<EClass>toSet(_collect_1);
-    int _size = _set.size();
-    InputOutput.<Integer>println(Integer.valueOf(_size));
+    InputOutput.<String>println("TODO");
     InputOutput.<String>println("Is there a search for each object type?");
-    Twitter _twitter = model.getTwitter();
-    EList<Type> _types = _twitter.getTypes();
-    final Function1<Type, Boolean> _function_5 = (Type it) -> {
-      return Boolean.valueOf((it instanceof TwitterObjectTypeDeclaration));
-    };
-    Iterable<Type> _select = de.hub.modsoft.twittersearch.main.IterableExtensions.<Type>select(_types, _function_5);
-    final Function1<Type, Boolean> _function_6 = (Type type) -> {
-      EList<Search> _searches_4 = model.getSearches();
-      final Function1<Search, Boolean> _function_7 = (Search it) -> {
-        TwitterObjectTypeDeclaration _searchType = it.getSearchType();
-        return Boolean.valueOf(Objects.equal(_searchType, type));
-      };
-      return Boolean.valueOf(IterableExtensions.<Search>exists(_searches_4, _function_7));
-    };
-    boolean _forall_1 = IterableExtensions.<Type>forall(_select, _function_6);
-    InputOutput.<Boolean>println(Boolean.valueOf(_forall_1));
+    InputOutput.<String>println("TODO");
     StringConcatenation _builder = new StringConcatenation();
     _builder.append("How many conditions does the whole model contain. ");
     _builder.newLine();
@@ -256,13 +138,6 @@ public class Main {
     _builder.append("All collection functions (collect, select, etc.) only work on iterables.");
     _builder.newLine();
     InputOutput.<String>println(_builder.toString());
-    TreeIterator<EObject> _eAllContents = model.eAllContents();
-    Iterable<EObject> _iterable = IteratorExtensions.<EObject>toIterable(_eAllContents);
-    final Function1<EObject, Boolean> _function_7 = (EObject it) -> {
-      return Boolean.valueOf((it instanceof Condition));
-    };
-    Iterable<EObject> _select_1 = de.hub.modsoft.twittersearch.main.IterableExtensions.<EObject>select(_iterable, _function_7);
-    int _size_1 = IterableExtensions.size(_select_1);
-    InputOutput.<Integer>println(Integer.valueOf(_size_1));
+    InputOutput.<String>println("TODO");
   }
 }
