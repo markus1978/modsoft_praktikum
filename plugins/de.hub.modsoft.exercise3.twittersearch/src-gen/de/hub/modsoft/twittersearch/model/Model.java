6f3c77689b8cd0436a072c8c8e57f1f4bc94053b
Markus Scheidgen <markus.scheidgen@gmail.com>
2017-05-15 08:26:51 +0200

    Prepared for SS-17

---
+++ plugins/de.hub.modsoft.exercise3.twittersearch/src-gen/de/hub/modsoft/twittersearch/model/Model.java
@@ -0,0 +1,18 @@
+/**
+ */
+package de.hub.modsoft.twittersearch.model;
+
+import org.eclipse.emf.ecore.EObject;
+
+/**
+ * <!-- begin-user-doc -->
+ * A representation of the model object '<em><b>Model</b></em>'.
+ * <!-- end-user-doc -->
+ *
+ *
+ * @see de.hub.modsoft.twittersearch.model.TwitterSearchPackage#getModel()
+ * @model
+ * @generated
+ */
+public interface Model extends EObject {
+} // Model
---
+++ plugins/de.hub.modsoft.exercise3.twittersearch/src-gen/de/hub/modsoft/twittersearch/model/impl/ModelImpl.java
@@ -0,0 +1,39 @@
+/**
+ */
+package de.hub.modsoft.twittersearch.model.impl;
+
+import de.hub.modsoft.twittersearch.model.Model;
+import de.hub.modsoft.twittersearch.model.TwitterSearchPackage;
+
+import org.eclipse.emf.ecore.EClass;
+
+import org.eclipse.emf.ecore.impl.MinimalEObjectImpl;
+
+/**
+ * <!-- begin-user-doc -->
+ * An implementation of the model object '<em><b>Model</b></em>'.
+ * <!-- end-user-doc -->
+ *
+ * @generated
+ */
+public class ModelImpl extends MinimalEObjectImpl.Container implements Model {
+	/**
+	 * <!-- begin-user-doc -->
+	 * <!-- end-user-doc -->
+	 * @generated
+	 */
+	protected ModelImpl() {
+		super();
+	}
+
+	/**
+	 * <!-- begin-user-doc -->
+	 * <!-- end-user-doc -->
+	 * @generated
+	 */
+	@Override
+	protected EClass eStaticClass() {
+		return TwitterSearchPackage.Literals.MODEL;
+	}
+
+} //ModelImpl
